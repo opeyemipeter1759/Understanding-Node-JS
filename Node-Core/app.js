297f6d8a77f5113c239ee9ad52e1855d94f465b6
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-09-28 04:25:46 -0700

    logging into a file

--- Node-Core/app.js
+++ Node-Core/app.js
@@ -28,7 +28,7 @@ rl.question(query, (answer)=>{
 // console.trace 
 
 var smallFunct = function () {
-    console.trace("let us  look at trace");
+    // console.trace("let us  look at trace");
 }
 
 var bigFunct = function () {
@@ -36,3 +36,15 @@ var bigFunct = function () {
 }
 
 bigFunct();
+
+
+const fs = require('fs');
+const { Console } = require('console');
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+const logger = new Console({ stdout: output, stderr: errorOutput });
+const number = 5;
+logger.log('number:', number);
+// In stdout.log: number 5
+const code = 9
+logger.error('error code:', code);
