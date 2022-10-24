b84b1e8327f715af5aa08709d848cbc7761fdb48
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-10-24 15:21:55 +0100

    Writing into a template file

--- Node-Core/nodejs-api/node-project/app.js
+++ Node-Core/nodejs-api/node-project/app.js
@@ -1,6 +1,7 @@
 const { createServer } = require('http');
 const url = require('url');
-const { readFileSync } = require('fs');
+const { readFileSync } = require( 'fs' );
+const replaceTemplate = require( './modules/replaceTemplate' );
 
 const tempOverview = readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -15,20 +16,7 @@ const tempProduct = readFileSync(
   'utf-8'
 );
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 const data = readFileSync('./dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
@@ -44,9 +32,13 @@ const server = createServer((req, res) => {
       .join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
-  } else if (pathname === '/product') {
-    res.end(data);
-    // console.log(data);
+  } else if ( pathname === '/product' )
+  {
+      res.writeHead( 200, { 'Content-type': 'text/html' } );
+      const product = dataObj[query.id]
+      const output = replaceTemplate(tempProduct, product)
+    res.end(output);
+    console.log(product);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
