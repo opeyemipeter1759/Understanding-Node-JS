a589eece1e05ac3bba0820905e43b554e9e44e6a
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-11-27 20:18:05 +0100

    adding route handler

--- Node-Core/ExpressProject/recipes/src/index.js
+++ Node-Core/ExpressProject/recipes/src/index.js
@@ -2,6 +2,13 @@ const express = require( "express" )
 
 const app = express()
 
+// route handler to send message to someone
+
+app.get( "/", ( req, res ) =>
+{
+    res.send("Hello Express Student")
+})
+
 const port = process.env.PORT || 5050
 
 app.listen( port, () =>
