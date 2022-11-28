94d221072ee37fe861e31f8d4fdafc3f33da1ee9
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-11-28 22:56:35 +0100

    complete set up

--- Node-Core/ExpressProject/recipes/src/index.js
+++ Node-Core/ExpressProject/recipes/src/index.js
@@ -1,12 +1,19 @@
 const express = require( "express" )
-
+const path = require('path')
 const app = express()
 
 // route handler to send message to someone
 
 app.get( "/", ( req, res ) =>
 {
-    res.send("Hello Express Student")
+    res.send( "Hello Express Student" )
+    // console.log(req);
+} )
+
+app.get( "/:name", ( req, res ) =>
+{
+    res.send( `welcome to Recipes${ req.params.name }` )
+    console.log(req.params);
 })
 
 const port = process.env.PORT || 5050
